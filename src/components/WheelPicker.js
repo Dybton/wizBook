639fd3f04df970962be8a0d3e027d1496102f8cf
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2021-03-07 15:31:45 +0100

    The static is basically done now. Next step is to setup authentificaiton and then the rest

--- src/components/WheelPicker.js
+++ src/components/WheelPicker.js
@@ -6,12 +6,13 @@ var PickerItem = Picker.Item;
 
 const WheelPicker = () => {
   const [selectedItem, setSelectedItem ] = useState(2);
-  const [itemList , setItemList ] = useState(['Page 1', 'Page 2', 'Page 3', 'Page 4', 'Page 5']);
+  const [itemList , setItemList ] = useState(['Page nr', '%', 'Page 3', 'Page 4', 'Page 5']);
 
   return (
     <View>
       <Text>
         <Picker style={{width: 150, height: 180}}
+          // I might need to custumize this - but lets see what Jonas finds
           lineColor="#000000" //to set top and bottom lissdssdsdne color (Without gradients)
           lineGradientColorFrom="#008000" //to set top and bottom starting gradient line color
           lineGradientColorTo="#FF5733" //to set top and bottom ending gradient
--- src/screens/ProgressScreen.js
+++ src/screens/ProgressScreen.js
@@ -2,21 +2,31 @@ import React from 'react';
 import { View, Text, Button, StyleSheet, TouchableOpacity, Image, TouchableOpacityBase } from 'react-native';
 import WheelPicker from '../components/WheelPicker';
 
+
 const ProgressScreen = ({ navigation }) => {
+
     return (
-      <View style={styles.question}>
-          <Text> How far have you read?  </Text>
-          <WheelPicker/> 
-          {/* <WheelPicker/>  */}
+      <View style={styles.container}>
+          <Text> How far have you reado?  </Text>
+          <View>
+            <WheelPicker/> 
+            {/* <WheelPicker/>  */}
+          </View>
           <Button title="Study!" onPress={() => navigation.navigate('StudyQuestions')} />
       </View>   
     );
 };
 
 const styles = StyleSheet.create({
-  question: {
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 120
   },
+  wheelContainer: {
+    flexDirection: 'row'
+  },
+
 });
 
 export default ProgressScreen;
